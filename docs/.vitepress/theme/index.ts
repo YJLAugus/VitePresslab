1302ebed342e1cd758940d81345376c16fe9d0e2
YJLAugus <yesyjl@163.com>
2025-06-26 23:18:44 +0800

    尝试彩虹动画

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,46 @@
 import DefaultTheme from 'vitepress/theme'
-import './custom.css'
-//import './rainbow.css'
-//import './overrides.css'
+import './custom.css' 
+import './rainbow.css'
+// import './overrides.css'
+// import './var.css'
+import { watch } from 'vue'
+// export default DefaultTheme
+/* .vitepress/theme/index.ts */ // [!code focus:3]
+// 彩虹背景动画样式
+let homePageStyle: HTMLStyleElement | undefined
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  enhanceApp({app , router }) {
+    // [!code focus:8]
+    // 彩虹背景动画样式
+    if (typeof window !== 'undefined') {
+      watch(
+        () => router.route.data.relativePath,
+        () => updateHomePageStyle(location.pathname === '/'),
+        { immediate: true },
+      )
+    }
+
+  },
+}
+// [!code focus:18]
+// 彩虹背景动画样式
+function updateHomePageStyle(value: boolean) {
+  if (value) {
+    if (homePageStyle) return
+
+    homePageStyle = document.createElement('style')
+    homePageStyle.innerHTML = `
+    :root {
+      animation: rainbow 12s linear infinite;
+    }`
+    document.body.appendChild(homePageStyle)
+  } else {
+    if (!homePageStyle) return
+
+    homePageStyle.remove()
+    homePageStyle = undefined
+  }
+}
+
